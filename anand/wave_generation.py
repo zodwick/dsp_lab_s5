8b5c1f322d982011ddefd50d0f2f01e5f4d8a649
zodwick <115868723+zodwick@users.noreply.github.com>
2023-11-15 20:40:01 +0530

    Fix pulse signal generation in wave_generation.py

--- anand/wave_generation.py
+++ anand/wave_generation.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 
 #discrete time
 
-# t = np.arange(-5,5,1)
-# x=np.zeros(len(t))
-# x[len(t)//2]=1
+t = np.arange(-5,5,1)
+x=np.zeros(len(t))
+x[len(t)//2]=1
 
-# plt.stem(t,x)
-# plt.show()
+plt.stem(t,x)
+plt.show()
 
-#continuous time
+# continuous time
 
 t = np.arange(-5,5,0.01)
 x=np.zeros(len(t))
@@ -25,6 +25,32 @@ plt.show()
 
 
 # 2. Unit pulse signal
+
+t= np.arange(-5,5,0.01)
+
+width = eval(input("Enter width of pulse: "))
+x=np.zeros(len(t))
+
+for i in range(len(t)):
+    if abs(t[i]) <= width/2:
+        x[i]=1
+        
+plt.plot(t,x)
+
+plt.show()
+
+# dt
+
+t=np.arange(-5,5,.2)
+width = eval(input("Enter width of pulse: "))
+x=np.zeros(len(t))
+for i in range(len(t)):
+    if abs(t[i]) <= width/2:
+        x[i]=1
+        
+plt.stem(t,x)
+plt.show()
+
 # 3. Unit ramp signal
 
 # 4. Bipolar pulse
