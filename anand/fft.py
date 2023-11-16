6e37a20cfb3174c298fa94a5ae570ec9e98d2a0a
zodwick <115868723+zodwick@users.noreply.github.com>
2023-11-16 21:00:48 +0530

    Add inverse FFT implementation

--- anand/fft.py
+++ anand/fft.py
@@ -13,9 +13,26 @@ def fft(x):
     
     return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
 
+
+def ifft(X):
+    N = len(X)
+    
+    if N <= 1:
+        return X
+    
+    even = ifft(X[0::2])
+    odd = ifft(X[1::2])
+    
+    T = [cmath.exp(2j * cmath.pi * k / N) * odd[k] for k in range(N // 2)]
+    
+    return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+
+
 # Example usage:
 if __name__ == "__main__":
     x = [0, 1, 2, 3, 4, 5, 6, 7]
     result = fft(x)
     print("FFT result:", result)
     print(numpy.fft.fft(x))
+    result_ifft = ifft(result)
+    print("IFFT result:", [elem / len(result) for elem in result_ifft])
